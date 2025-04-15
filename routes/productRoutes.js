771a2115b47013d28ca9cf647a2f2371a0e74a86
uraghuvamshireddy <uraghuvamshireddy@gmail.com>
2025-04-15 16:11:02 +0530

    added images

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,11 +8,5 @@ const router = express.Router();
 router.post('/add-product/:firmId',productController.addProduct);
 router.get('/:firmId/products',productController.getProductByFirm);
 router.delete('/:productId',productController.deletedProductById);
-router.get('/uploads/:imageName',(req,res)=>{
-    const imageName = req.params.imageName;
-    res.headersSent('Content-Type','image/jpeg');
-    res.sendFile(path.join(__dirname,'..','uploads',imageName));
-
-})
 
 module.exports = router;
